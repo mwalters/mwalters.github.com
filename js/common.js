0b1bfd59a781288ccb28522dd53b9462690bee44
Matt Walters <mwalters8@gmail.com>
2013-04-10 20:59:54 -0400

    Updates

--- js/common.js
+++ js/common.js
@@ -25,17 +25,4 @@ $(document).ready(function () {
     titletag: false,
     date: false
   });
-	$(function() {
-		$.ajax({
-			type: "GET",
-			dataType: "jsonp",
-			cache: false,
-			url: "https://api.instagram.com/v1/users/355730/media/recent/?access_token=18360510.f59def8.d8d77acfa353492e8842597295028fd3",
-			success: function(data) {
-				for (var i = 0; i < 1; i++) {
-          $("#instagram").append("<div class='instagram-placeholder'><a target='_blank' href='" + data.data[i].link +"'><img class='instagram-image' src='" + data.data[i].images.low_resolution.url +"' /></a></div>");
-        }
-			}
-		});
-	});
 });
